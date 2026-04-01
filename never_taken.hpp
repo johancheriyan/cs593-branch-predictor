2e4b818f25b61e57e17c9130749941120b88c27d
Johan Cheriyan <johancheriyan@gmail.com>
2026-04-01 13:27:19 +0000

    Task 1: Make never_taken a template class

--- never_taken.hpp
+++ never_taken.hpp
@@ -1,34 +1,28 @@
 #include "../cbp.hpp"
 #include "../harcom.hpp"
-
 using namespace hcm;
-
+template<int DUMMY=0>
 struct never_taken : predictor {
     val<1> predict1([[maybe_unused]] val<64> inst_pc)
     {
         reuse_prediction(hard<1>{});
         return hard<0>{};
     };
-
     val<1> reuse_predict1([[maybe_unused]] val<64> inst_pc)
     {
         reuse_prediction(hard<1>{});
         return hard<0>{};
     };
-
     val<1> predict2([[maybe_unused]] val<64> inst_pc)
     {
         reuse_prediction(hard<1>{});
         return hard<0>{};
     }
-
     val<1> reuse_predict2([[maybe_unused]] val<64> inst_pc)
     {
         reuse_prediction(hard<1>{});
         return hard<0>{};
     }
-
     void update_condbr([[maybe_unused]] val<64> branch_pc, [[maybe_unused]] val<1> taken, [[maybe_unused]] val<64> next_pc) { }
-
     void update_cycle([[maybe_unused]] instruction_info &block_end_info) { }
 };
